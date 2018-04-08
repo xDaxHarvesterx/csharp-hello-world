1f1741b18cad605fdcf895c6f36b967758d6d9d7
xDaxHarvesterx <38172334+xDaxHarvesterx@users.noreply.github.com>
2018-04-07 22:04:01 -0600

    Add files via upload

---
+++ Hello World.cs
@@ -0,0 +1,12 @@
+//Using the Consoles system
+using System; //Provides easier writing access
+
+//The public program
+public class Program
+{
+	//Main method
+	public static void Main()
+	{
+		Console.WriteLine("Hello World!");
+	}
+}
